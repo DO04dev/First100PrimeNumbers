40b06eb4724e127d6f98c4bd78295f0763ea2c1a
Reinnovs <33583262+reinnovs@users.noreply.github.com>
2020-09-02 16:46:31 +0200

    Project

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace First100PrimeNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int value = 0;
+            int rowDisplayCount = 0;
+
+            Console.WriteLine("|The First 100 Prime Numbers are:|");
+            Console.WriteLine("|********************************|\n");
+
+            do
+            {
+                int num = value % 2;
+
+                if(num != 0)
+                {
+                    Console.Write("{0} ", value);
+                    rowDisplayCount++;
+
+                    //Display Only 10 values per row
+                    if (rowDisplayCount == 10)
+                    {
+                        Console.WriteLine("\n");
+                        rowDisplayCount = 0;
+                    }
+
+                    count++;
+                }
+                value++;
+
+            } while (count <= 100);
+
+            Console.ReadKey();
+        }
+    }
+}
